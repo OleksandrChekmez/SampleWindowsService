83cfb04a541df7034e7b638b37fe4a25926f1df9
Oleksandr Chekmez <chekmez@nikama.com>
2016-05-19 13:13:39 +0300

    add code

---
+++ CommandWindowsService/TestCommandService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+
+namespace CommandWindowsService
+{
+    public partial class TextCommandService : ServiceBase
+    {
+        
+        public TextCommandService()
+        {
+            InitializeComponent();
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Update the service state to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+                        
+            // Set up a timer to trigger every minute.
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 10000; // 10 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnStop()
+        {
+            
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            
+            
+            this.RunScript(@"c:\start.bat");
+        }
+
+        private void RunScript(string processFileName)
+        {            
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = @"/C c:\start.bat",
+                    CreateNoWindow = true,
+                    ErrorDialog = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+
+
+                var process = new Process();
+                process.StartInfo = startInfo;
+                process.Start();
+         
+         
+        }
+
+        protected override void OnContinue()
+        {
+            
+        }
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public long dwServiceType;
+            public ServiceState dwCurrentState;
+            public long dwControlsAccepted;
+            public long dwWin32ExitCode;
+            public long dwServiceSpecificExitCode;
+            public long dwCheckPoint;
+            public long dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SampleWindowsService
+Sample Windows service which executes batch file every 10 seconds. 
+Batch files should be in c:\start.bat
